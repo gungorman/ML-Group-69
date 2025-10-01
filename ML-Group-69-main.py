a827153c248f2e910d649dafac884dcb4232253a
lucasggxx <lg7rm1@gmail.com>
2025-10-01 14:10:39 +0200

    1try
    cheeks

--- ML-Group-69-main.py
+++ ML-Group-69-main.py
@@ -5,3 +5,6 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import SGDRegressor
 
 import matplotlib.pyplot as plt
+
+data = pd.read_csv('football_wages.csv')
+data.head()
